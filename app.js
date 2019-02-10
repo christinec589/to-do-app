fd7c8707cc0e2e728670648db0952551fdd68274
Christine Chiang <christinec.589@gmail.com>
2019-02-10 16:45:26 -0600

    to-do-app js edit

--- app.js
+++ app.js
@@ -1,49 +1,45 @@
 function onReady() { 
 
+    const toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
- 
-    addToDoForm.addEventListener('submit', (event) => {
-        event.preventDefault();
 
-        //get the text
-        let title = newToDoText.value;
+    function createNewToDo() {
+        const newToDoText = document.getElementById('newToDoText');
+        if (!newToDoText.value) {return;}
 
-        //create a new li
-        let newLi = document.createElement('li');
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
 
-        //create a new input
-        let checkbox = document.createElement('input');
-
-        //create delete button
-        let deleteButton = document.createElement('button');
-
-        //set the input's type to checkbox
-        checkbox.type = "checkbox";
-
-        //set the button text
-        deleteButton.textContent = 'Delete';
-
-        //set the title
-        newLi.textContent = title;
-
-        //attach the textbox to the li
-        newLi.appendChild(checkbox);
-
-        //attach the delete button to the li
-        newLi.appendChild(deleteButton);
-
-        //attach the li to the ul
-        toDoList.appendChild(newLi);
-
-        //empty the input
         newToDoText.value = '';
 
-        deleteButton.addEventListener('click', (event) => {
-            toDoList.removeChild(newLi); 
-        }); 
-    });  
+        renderTheUI();
+    }
+
+    function renderTheUI(){
+        const toDoList = document.getElementById('toDoList');
+
+        toDoList.textContent = '';
+        
+        toDos.forEach(function(toDo){
+            const newLi = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+
+            newLi.textContent = toDo.title;
+
+            toDoList.appendChild(newLi);
+            newLi.appendChild(checkbox);
+        });
+    }
+
+    addToDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createNewToDo();
+    });
+
+    renderTheUI();
 
 }
 
